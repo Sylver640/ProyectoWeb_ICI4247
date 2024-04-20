953126a11df6475f7b8cd73ba1f525a5258ac417
LotaSchwager <ademir.munoz.r@mail.pucv.cl>
2024-04-19 23:22:37 -0400

    Reordenando para ser compatible con el main

---
+++ maquetacion/src/pages/menu_pages/Library.tsx
@@ -0,0 +1,11 @@
+import { IonContent, IonText } from "@ionic/react";
+
+const library = () => {
+    return (
+        <IonContent>
+            <IonText>Libreria</IonText>
+        </IonContent>
+    );
+}
+
+export default library;
---
+++ maquetacion/src/pages/menu_pages/Search.tsx
@@ -0,0 +1,45 @@
+import { IonInput, IonContent, IonCardContent, IonList, IonItem, IonThumbnail, IonLabel, IonCardSubtitle } from "@ionic/react";
+import '../../styles/menu styles/Search.css';
+import {cards} from "../../components/cards";
+
+
+const Search = () => {
+    return (
+        <IonContent>
+
+            <div className="contenedor-search">
+
+                <IonItem lines="full" className="search-bar">
+                    <IonInput
+                        type="text"
+                        placeholder="Search for a song"
+                    ></IonInput>
+                </IonItem>
+            
+
+                <p className="text-search">Recent searches</p>
+
+                <div className="contenedor-songs">    
+                    <IonCardContent className="search-list">
+                        <IonList className="search-list">
+
+                            {cards.map((card) => {
+                                return(
+                                    <IonItem key={card.id} button={true} className="song-searched">
+                                        <IonThumbnail slot="start">
+                                                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+                                        </IonThumbnail>
+                                        <IonLabel>{card.title}</IonLabel>
+                                    </IonItem>
+                                    );
+                                })}
+
+                        </IonList>
+                    </IonCardContent>
+                </div>
+            </div>
+        </IonContent>
+    );
+}
+
+export default Search;
